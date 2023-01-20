b3ac3d26957121c542adaaa78a96f04efc1544fb
johnsonjason099 <johnsonjason099@gmail.com>
2023-01-20 11:17:02 -0700

    first commit

---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+import posts from './posts';
+//Lorivie
+//const reducers =combineReducers({ posts });
+//export default reducers;
+
+
+export default combineReducers({ posts });
+
